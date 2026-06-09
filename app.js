fa883addcfc6cfc63b8ab20b7240bc1ddeb6944c
Ashok Varma <ashokvarma5247@gmail.com>
2026-06-09 23:56:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const mongoose = require("mongoose");
 
 mongoose.connect(
-"mongodb://ashokvarma5247_db_user:Ashok%40254@ac-l4icr6m-shard-00-00.siesasy.mongodb.net:27017,ac-l4icr6m-shard-00-01.siesasy.mongodb.net:27017,ac-l4icr6m-shard-00-02.siesasy.mongodb.net:27017/?ssl=true&replicaSet=atlas-neymvt-shard-0&authSource=admin&appName=Cluster0"
+"mongodb://ashokvarma5247_db_user:Ashok@254@ac-l4icr6m-shard-00-00.siesasy.mongodb.net:27017,ac-l4icr6m-shard-00-01.siesasy.mongodb.net:27017,ac-l4icr6m-shard-00-02.siesasy.mongodb.net:27017/?ssl=true&replicaSet=atlas-neymvt-shard-0&authSource=admin&appName=Cluster0"
 )
 .then(() => {
     console.log("✅ MongoDB Connected");
